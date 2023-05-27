0b38a9051a7e2562800a4694cc01146e399d6841
iam454 <realseogy@gmail.com>
2023-05-27 17:03:48 +0900

    feat: search 생성 및 css작업

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -9,11 +9,11 @@ export default class Headline extends Component {
         THE OPEN<br />
         MOVIES DATABASE
       </h1>
-    <p>
-      The OMDb API is a RESTful web service to obtain movie information,
-      all content and images on the site are contributed and maintained by our users.<br />
-      If you find this service useful, please consider making a one-time donation or become a patron.
-    </p>
+      <p>
+        The OMDb API is a RESTful web service to obtain movie information,
+        all content and images on the site are contributed and maintained by our users.<br />
+        If you find this service useful, please consider making a one-time donation or become a patron.
+      </p>
     `;
   }
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,13 @@
 import { Component } from "../core/heropy.js";
 import Headline from "../components/Headline.js";
+import Search from "../components/Search.js";
 
 export default class Home extends Component {
   render() {
     const headline = new Headline().el;
+    const search = new Search().el;
+
     this.el.classList.add("container");
-    this.el.append(headline);
+    this.el.append(headline, search);
   }
 }
