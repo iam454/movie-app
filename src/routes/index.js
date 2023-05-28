6c312186d220ff54d8a0cf687beeaaf914ecb84f
iam454 <realseogy@gmail.com>
2023-05-28 10:42:35 +0900

    feat: 영화 상세 정보 페이지 생성

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
 import { createRouter } from "../core/heropy.js";
 import Home from "./Home.js";
+import Movie from "./Movie.js";
 
 export default createRouter([
   {
     path: "#/",
     component: Home,
+  },
+  {
+    path: "#/movie",
+    component: Movie,
   },
 ]);
